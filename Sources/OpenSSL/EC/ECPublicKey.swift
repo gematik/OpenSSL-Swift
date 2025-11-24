21acce5ac60ea0d5f57f88aca1e905e4a5ba93a5
Gerald Bartz <gerald.bartz@gematik.de>
2025-11-24 09:21:30 +0100

    Change license to Apache and OSPO policy compliance

--- Sources/OpenSSL/EC/ECPublicKey.swift
+++ Sources/OpenSSL/EC/ECPublicKey.swift
@@ -1,20 +1,21 @@
 //
-// Copyright 2016, European Community
-// Copyright 2023, gematik GmbH
-
-// Licensed under the EUPL, Version 1.2 or - as soon they will be approved by the
-// European Commission – subsequent versions of the EUPL (the "Licence").
-// You may not use this work except in compliance with the Licence.
-
-// You find a copy of the Licence in the "Licence" file or at
-// https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
-
-// Unless required by applicable law or agreed to in writing,
-// software distributed under the Licence is distributed on an "AS IS" basis,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either expressed or implied.
-// In case of changes by gematik find details in the "Readme" file.
-
-// See the Licence for the specific language governing permissions and limitations under the Licence.
+// Copyright (Change Date see Readme), gematik GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// *******
+//
+// For additional notes and disclaimer from gematik and in case of changes by gematik find details in the "Readme" file.
 //
 
 @_implementationOnly import COpenSSL
